4bd85248fd5cabcf6f00d2aedb651aba87d370bf
DoaaEssam52 <due_essam_2015@yahoo.com>
2018-11-22 20:32:37 +0200

    First commit

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include<stdlib.h>
+using namespace std;
+char Xor (char x , char y)
+{
+    if((x=='0'&&y=='0')||(x=='1'&&y=='1'))
+        return '0';
+    else
+        return '1';
+}
+string alter(int index,string m_in)
+{   cout<<" Index = ";
+    cin>>index;
+    if(index!=0)
+        {
+    if(m_in[index]=='0')
+    m_in[index]='1';
+    else
+    m_in[index]='0';
+        }
+        else
+        {
+
+        cout<<"Index should not equal 0"<<endl;
+        m_in=alter(index,m_in);
+        }
+    return m_in;
+
+}
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
